6a88130c05cb1ca6d6ea37a834f1f0d2ba61c8a8
Danil Bazhenov <dbbazhenov@miem.hse.ru>
2021-06-16 19:42:26 +0300

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/krestiki.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/krestiki.dir/main.cpp.obj"
+  "krestiki.exe"
+  "krestiki.exe.manifest"
+  "krestiki.pdb"
+  "libkrestiki.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/krestiki.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
