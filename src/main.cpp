bbcc83760407bdaa3b7d42d7d6af44c3a93eeae3
Danil Bazhenov <dbbazhenov@miem.hse.ru>
2021-06-18 15:12:10 +0300

    last_last

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,3 @@
-#include "nana/gui/widgets/picture.hpp"
-#include "nana/paint/image.hpp"
-#include <fmt/core.h>
 #include <nana/gui.hpp>
 #include <nana/gui/widgets/button.hpp>
 #include <nana/gui/widgets/label.hpp>
@@ -10,54 +7,63 @@
 /*
 функция, проверяющая возможную выигрышную комбинацию
 
-Если конмбинация выигрыщная, то заканчивает игру и подсвечитвает выигрыш красным
+Если конмбинация выигрышная, то заканчивает игру и подсвечитвает выигрыш красным
 */
 void fn(std::string (&battlefield)[9], bool &game_run, nana::button (&button_all)[9]){
+//использование пространства имен "nana"
     using namespace nana;
-    
+//проверка первой строки
     if ((battlefield[0] == battlefield[1]) && (battlefield[1] == battlefield[2]) && battlefield[2] != "") {
+//заверишить игру
             game_run = false;
+//перекрасить выигрушную комбинацию в красный цвет
             button_all[0].bgcolor(color_rgb(0xDC143C));
             button_all[1].bgcolor(color_rgb(0xDC143C));
             button_all[2].bgcolor(color_rgb(0xDC143C));
         }
-        else if((battlefield[3] == battlefield[4]) & (battlefield[4] == battlefield[5]) & battlefield[5] != "") {
+//проверка второй строки
+        else if((battlefield[3] == battlefield[4]) && (battlefield[4] == battlefield[5]) && battlefield[5] != "") {
             game_run = false;
             button_all[3].bgcolor(color_rgb(0xDC143C));
             button_all[4].bgcolor(color_rgb(0xDC143C));
             button_all[5].bgcolor(color_rgb(0xDC143C));
         }
-        else if((battlefield[6] == battlefield[7]) & (battlefield[7] == battlefield[8]) & battlefield[8] != "") {
+//проверка третьей строки
+        else if((battlefield[6] == battlefield[7]) && (battlefield[7] == battlefield[8]) && battlefield[8] != "") {
             game_run = false;
             button_all[6].bgcolor(color_rgb(0xDC143C));
             button_all[7].bgcolor(color_rgb(0xDC143C));
             button_all[8].bgcolor(color_rgb(0xDC143C));
         }
-        else if((battlefield[0] == battlefield[3]) & (battlefield[3] == battlefield[6]) & battlefield[6] != "") {
+//проверка первого столбца
+        else if((battlefield[0] == battlefield[3]) && (battlefield[3] == battlefield[6]) && battlefield[6] != "") {
             game_run = false;
             button_all[0].bgcolor(color_rgb(0xDC143C));
             button_all[3].bgcolor(color_rgb(0xDC143C));
-            button_all[7].bgcolor(color_rgb(0xDC143C));
+            button_all[6].bgcolor(color_rgb(0xDC143C));
         }
-        else if((battlefield[1] == battlefield[4]) & (battlefield[4] == battlefield[7]) & battlefield[7] != "") {
+//проверка второго столбца
+        else if((battlefield[1] == battlefield[4]) && (battlefield[4] == battlefield[7]) && battlefield[7] != "") {
             game_run = false;
             button_all[1].bgcolor(color_rgb(0xDC143C));
             button_all[4].bgcolor(color_rgb(0xDC143C));
             button_all[7].bgcolor(color_rgb(0xDC143C));
         }
-        else if((battlefield[2] == battlefield[5]) & (battlefield[5] == battlefield[8]) & battlefield[8] != "") {
+//проверка третьего столбца
+        else if((battlefield[2] == battlefield[5]) && (battlefield[5] == battlefield[8]) && battlefield[8] != "") {
             game_run = false;
             button_all[2].bgcolor(color_rgb(0xDC143C));
             button_all[5].bgcolor(color_rgb(0xDC143C));
             button_all[8].bgcolor(color_rgb(0xDC143C));
         }
-        else if((battlefield[0] == battlefield[4]) & (battlefield[4] == battlefield[8]) & battlefield[8] != "") {
+//проверка диагоналей
+        else if((battlefield[0] == battlefield[4]) && (battlefield[4] == battlefield[8]) && battlefield[8] != "") {
             game_run = false;
             button_all[0].bgcolor(color_rgb(0xDC143C));
             button_all[4].bgcolor(color_rgb(0xDC143C));
             button_all[8].bgcolor(color_rgb(0xDC143C));
         }
-        else if((battlefield[2] == battlefield[4]) & (battlefield[4] == battlefield[6]) & battlefield[6] != "") {
+        else if((battlefield[2] == battlefield[4]) && (battlefield[4] == battlefield[6]) && battlefield[6] != "") {
             game_run = false;
             button_all[2].bgcolor(color_rgb(0xDC143C));
             button_all[4].bgcolor(color_rgb(0xDC143C));
@@ -72,7 +78,7 @@ void fn(std::string (&battlefield)[9], bool &game_run, nana::button (&button_all
 */
 void clear(nana::button (&button_all)[9]){
     using namespace nana;
-
+//отчистка текста кнопок
     button_all[0].caption("");
     button_all[1].caption("");
     button_all[2].caption("");
@@ -82,6 +88,7 @@ void clear(nana::button (&button_all)[9]){
     button_all[6].caption("");
     button_all[7].caption("");
     button_all[8].caption("");
+//отчистка цвета кнопок
     button_all[0].bgcolor(color_rgb(0xFFFFFF));
     button_all[1].bgcolor(color_rgb(0xFFFFFF));
     button_all[2].bgcolor(color_rgb(0xFFFFFF));
@@ -93,7 +100,9 @@ void clear(nana::button (&button_all)[9]){
     button_all[8].bgcolor(color_rgb(0xFFFFFF));
 }
 
-
+/*
+"Тело" игры
+*/
 int main() {
     using namespace nana;
     
@@ -143,7 +152,7 @@ int main() {
         Проверяет, продолжается ли игра и пустая ли клетка
         Ставит крестик или нолик в зависимости от хода
         */
-        if (battlefield[0] == "" & game_run == true) {
+        if (battlefield[0] == "" && game_run == true) {
             battlefield[0] = btn_txt;
             button_all[0].caption(btn_txt);
             if (btn_txt == "X")
@@ -162,7 +171,7 @@ int main() {
     button_all[1].bgcolor(color_rgb(0xFFFFFF));
     button_all[1].typeface(nana::paint::font("", 40, true));
     button_all[1].events().click([&] {
-        if (battlefield[1] == "" & game_run == true) {
+        if (battlefield[1] == "" && game_run == true) {
             battlefield[1] = btn_txt;
             button_all[1].caption(btn_txt);
             if (btn_txt == "X")
@@ -177,7 +186,7 @@ int main() {
     button_all[2].bgcolor(color_rgb(0xFFFFFF));
     button_all[2].typeface(nana::paint::font("", 40, true));
     button_all[2].events().click([&] {
-        if (battlefield[2] == "" & game_run == true) {
+        if (battlefield[2] == "" && game_run == true) {
             battlefield[2] = btn_txt;
             button_all[2].caption(btn_txt);
             if (btn_txt == "X")
@@ -192,7 +201,7 @@ int main() {
     button_all[3].bgcolor(color_rgb(0xFFFFFF));
     button_all[3].typeface(nana::paint::font("", 40, true));
     button_all[3].events().click([&] {
-        if (battlefield[3] == "" & game_run == true) {
+        if (battlefield[3] == "" && game_run == true) {
             battlefield[3] = btn_txt;
             button_all[3].caption(btn_txt);
             if (btn_txt == "X")
@@ -207,7 +216,7 @@ int main() {
     button_all[4].bgcolor(color_rgb(0xFFFFFF));
     button_all[4].typeface(nana::paint::font("", 40, true));
     button_all[4].events().click([&] {
-        if (battlefield[4] == "" & game_run == true) {
+        if (battlefield[4] == "" && game_run == true) {
             battlefield[4] = btn_txt;
             button_all[4].caption(btn_txt);
             if (btn_txt == "X")
@@ -222,7 +231,7 @@ int main() {
     button_all[5].bgcolor(color_rgb(0xFFFFFF));
     button_all[5].typeface(nana::paint::font("", 40, true));
     button_all[5].events().click([&] {
-        if (battlefield[5] == "" & game_run == true) {
+        if (battlefield[5] == "" && game_run == true) {
             battlefield[5] = btn_txt;
             button_all[5].caption(btn_txt);
             if (btn_txt == "X")
@@ -237,7 +246,7 @@ int main() {
     button_all[6].bgcolor(color_rgb(0xFFFFFF));
     button_all[6].typeface(nana::paint::font("", 40, true));
     button_all[6].events().click([&] {
-        if (battlefield[6] == "" & game_run == true) {
+        if (battlefield[6] == "" && game_run == true) {
             battlefield[6] = btn_txt;
             button_all[6].caption(btn_txt);
             if (btn_txt == "X")
@@ -252,7 +261,7 @@ int main() {
     button_all[7].bgcolor(color_rgb(0xFFFFFF));
     button_all[7].typeface(nana::paint::font("", 40, true));
     button_all[7].events().click([&] {
-        if (battlefield[7] == "" & game_run == true) {
+        if (battlefield[7] == "" && game_run == true) {
             battlefield[7] = btn_txt;
             button_all[7].caption(btn_txt);
             if (btn_txt == "X")
@@ -267,7 +276,7 @@ int main() {
     button_all[8].bgcolor(color_rgb(0xFFFFFF));
     button_all[8].typeface(nana::paint::font("", 40, true));
     button_all[8].events().click([&] {
-        if (battlefield[8] == "" & game_run == true) {
+        if (battlefield[8] == "" && game_run == true) {
             battlefield[8] = btn_txt;
             button_all[8].caption(btn_txt);
             if (btn_txt == "X")
@@ -300,9 +309,11 @@ int main() {
     change_mod.typeface(nana::paint::font("", 15, true));
     change_mod.caption("vs PC");
     change_mod.events().click([&] {
+//если выключен pvp мод, то включаем его
         if (!pvp_mod) {
             change_mod.caption("PvP");
             pvp_mod = true;
+//если был включен pvp, то включаем режим против пк
         } else {
             change_mod.caption("vs PC");
             pvp_mod = false;
@@ -320,10 +331,6 @@ int main() {
     button_exit.bgcolor(color_rgb(0xDC143C));
     button_exit.typeface(nana::paint::font("", 15, true));
     button_exit.events().click([&fm] { fm.close(); });
-
-    button_all[6].caption(battlefield[0]);
-    button_all[7].caption(battlefield[1]);
-    button_all[8].caption(battlefield[2]);
 
     //Настройка расположений кнопок
     fm.div("vert <>"
